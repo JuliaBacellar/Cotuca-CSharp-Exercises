b76d0f8040fe71033c1a9171d7cbc6c240532501
juliab23526 <cc23526@g.unicamp.br>
2025-04-07 19:26:59 -0300

    31.03

---
+++ apListaLigada_1_a_4/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace apListaLigada_1_a_4
+{
+    public partial class FrmListaLigada : Form
+    {
+        ListaSimples<Aluno> lista1;
+        public FrmListaLigada()
+        {
+            InitializeComponent();
+        }
+
+        // tratador do evento Load
+        // esse evento ocorre quanto o formulário está criado
+        // na memória e logo será exibido (antes de ser exibido)
+        private void FrmListaLigada_Load(object sender, EventArgs e)
+        {
+            lista1 = new ListaSimples<Aluno>();
+        }
+
+        // esse método será chamado automaticamente quando o usuário
+        // clicar no btnIncluir
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            if (txtRA.Text != "" && txtNome.Text != "")
+            {
+                var novoAluno = new Aluno(txtRA.Text, txtNome.Text,
+                                            (double)udNota.Value);
+                lista1.InserirAposFim(novoAluno);
+                lista1.Listar(lsbUm);
+            }
+        }
+
+        private void btnLerArquivo1_Click(object sender, EventArgs e)
+        {
+            if (dlgAbrir.ShowDialog() == DialogResult.OK)
+            {
+                lista1 = new ListaSimples<Aluno>();
+                var arquivo = new StreamReader(dlgAbrir.FileName);
+                while (!arquivo.EndOfStream)
+                {
+                    var linhaLida = arquivo.ReadLine();
+                    var novoAluno = new Aluno(linhaLida);
+                    lista1.InserirAposFim(novoAluno);
+                }
+                arquivo.Close();
+                lista1.Listar(lsbUm);
+            }
+        }
+    }
+}
---
+++ apListaLigada_1_a_4/ListaSimples.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Windows.Forms;
+
+public class ListaSimples<Dado> where Dado : IComparable<Dado>
+{
+    NoLista<Dado> primeiro, ultimo, atual, anterior;
+
+    int quantosNos;
+
+    public ListaSimples()
+    {
+        primeiro = ultimo = null;
+        quantosNos = 0;
+    }
+
+    public bool EstaVazia
+    {
+        get => primeiro == null;
+
+        // get 
+        // {
+        //    if (primeiro == null)
+        //       return true;
+        //    return false;
+        // }
+    }
+    public void InserirAposFim(Dado novoDado)
+    {
+        NoLista<Dado> novoNo = new NoLista<Dado>(novoDado);
+
+        if (EstaVazia)
+            primeiro = novoNo;
+        else
+            ultimo.Prox = novoNo;
+
+
+        ultimo = novoNo;
+        quantosNos++;
+    }
+
+    public void InserirAntesDoInicio(Dado novoDado)
+    {
+        var novoNo = new NoLista<Dado>(novoDado);
+
+        if (EstaVazia)          // se a lista está vazia, estamos
+            ultimo = novoNo;    // incluindo o 1o e o último nós!
+        else
+            novoNo.Prox = primeiro;
+
+        primeiro = novoNo;
+        quantosNos++;
+    }
+
+    public void ExibirNaTela()
+    {
+        atual = primeiro;
+        while (atual != null)
+        {
+            Console.WriteLine(atual.Info);
+            atual = atual.Prox;
+        }
+    }
+
+    public void Listar(ListBox oListBox)
+    {
+        oListBox.Items.Clear(); // limpa o conteúdo do listBox
+        atual = primeiro;       // posiciona ponteiro no 1o nó da lista
+        while (atual != null)   // percorre a lista ligada até seu final
+        {
+            oListBox.Items.Add(atual.Info); // exibe os itens da lista ligada no listbox
+            atual = atual.Prox;             // avança para o nó seguinte
+        }
+    }
+
+}
